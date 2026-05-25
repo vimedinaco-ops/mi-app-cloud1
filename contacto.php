23191472301ed6cb7f7062a2dd8e445e31405a04
Victor <tu_correo@ejemplo.com>
2026-05-25 09:59:11 -0500

    Agregar contacto

---
+++ contacto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Contacto</title>
+</head>
+<body>
+
+<h1>Formulario de Contacto</h1>
+
+<form>
+
+<input type="text" placeholder="Nombre"><br><br>
+
+<input type="email" placeholder="Correo"><br><br>
+
+<textarea placeholder="Mensaje"></textarea><br><br>
+
+<button type="submit">
+Enviar
+</button>
+
+</form>
+
+</body>
+</html>
